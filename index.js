6f10b271d2fb6496707f6641c5a3699984040ff4
kareemAlqubati <salamkareem792@gmail.com>
2024-08-20 19:36:08 +0300

    complete

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-
 // Write your code here
+
+let num1 = 7;
+let num2 = 9;
+let multiply = num1 * num2-1; 
+
+let random = Math.floor(Math.random() * 100) + 1; 
+
+let num3 = 22;
+let num4 = 6;
+let mod = num3 % num4; 
+
+let values = [10, 5, 20, 12];
+let max = Math.max(...values); 
+
+console.log("Multiplication result:", multiply);
+console.log("Random integer:", random);
+console.log("Remainder:", mod);
+console.log("Highest number:", max);
